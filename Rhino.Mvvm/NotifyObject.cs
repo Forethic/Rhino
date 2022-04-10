48e999c60e93ed8926870e1b02595fa4bdb5600d
Thx_Lia <jonathan.lia@outlook.com>
2022-04-10 11:55:51 +0800

    WPF (net framework) MVVM 基础框架

---
+++ Rhino.Mvvm/NotifyObject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Rhino.Mvvm
+{
+    /// <summary>
+    /// 实现了 <see cref="INotifyPropertyChanged"/> 的通知类
+    /// </summary>
+    public class NotifyObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// 仅当field与value不相等时更新属性值，并且通知属性值发生变化
+        /// </summary>
+        /// <typeparam name="T">object</typeparam>
+        /// <param name="field">属性的对应字段</param>
+        /// <param name="value">属性的新值</param>
+        /// <param name="updateBeforeAction">更新之前的操作</param>
+        /// <param name="updateAfterAction">更新之后的操作</param>
+        /// <param name="propertyName">通知变化的属性名称</param>
+        protected void Update<T>(ref T field, T value, Action<T, T> updateBeforeAction = null, Action<T, T> updateAfterAction = null, [CallerMemberName] string propertyName = null)
+        {
+            updateBeforeAction?.Invoke(field, value);
+            if (!Equals(field, value))
+            {
+                field = value;
+            }
+            updateAfterAction?.Invoke(field, value);
+            RaisePropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// 强制更新属性值，并且通知属性值发生变化
+        /// </summary>
+        /// <typeparam name="T">object</typeparam>
+        /// <param name="field">属性的对应字段</param>
+        /// <param name="value">属性的新值</param>
+        /// <param name="updateBeforeAction">更新之前的操作</param>
+        /// <param name="updateAfterAction">更新之后的操作</param>
+        /// <param name="propertyName">通知变化的属性名称</param>
+        protected void Coerce<T>(ref T field, T value, Action<T, T> updateBeforeAction = null, Action<T, T> updateAfterAction = null, [CallerMemberName] string propertyName = null)
+        {
+            updateBeforeAction?.Invoke(field, value);
+            field = value;
+            updateAfterAction?.Invoke(field, value);
+            RaisePropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// 通知属性值发生变化
+        /// </summary>
+        /// <param name="propertyName">通知变化的属性名称</param>
+        public void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Rhino.Mvvm/RelayCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Rhino.Mvvm
+{
+    /// <summary>
+    /// 实现了 ICommand 的响应命令类
+    /// </summary>
+    public class RelayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private readonly Action<object> _action;
+        private readonly Predicate<object> _predicate = (obj) => true;
+
+        public RelayCommand(Action<object> action, Predicate<object> predicate = null)
+        {
+            _action = action;
+            if (predicate != null)
+            {
+                _predicate = predicate;
+            }
+        }
+
+        public bool CanExecute(object parameter) => _predicate(parameter);
+
+        public void Execute(object parameter) => _action(parameter);
+    }
+}
