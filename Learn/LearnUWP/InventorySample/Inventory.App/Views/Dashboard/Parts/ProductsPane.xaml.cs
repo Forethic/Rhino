0536f6c53f4edca96bb13f1ff1af3851c25d098b
Jonathan <Forethic@outlook.com>
2022-10-08 23:14:38 +0800

    Learn UWP - Inventory PART 3.3 - Dashboard Part - ProductsPane

---
+++ Learn/LearnUWP/InventorySample/Inventory.App/Views/Dashboard/Parts/ProductsPane.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Inventory.Models;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+
+namespace Inventory.Views
+{
+    public sealed partial class ProductsPane
+    {
+        public IList<ProductModel> ItemsSource
+        {
+            get => (IList<ProductModel>)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty
+            = DependencyProperty.Register(nameof(ItemsSource), typeof(IList<ProductModel>), typeof(ProductsPane), new PropertyMetadata(null));
+
+        public ProductsPane()
+        {
+            InitializeComponent();
+
+            // 测试数据
+            ItemsSource = new List<ProductModel>()
+            {
+                new ProductModel
+                {
+                    ProductID = "10086",
+                    Name = "测试商品",
+                    ListPrice = 180m,
+                    ThumbnailBitmap = new Windows.UI.Xaml.Media.Imaging.BitmapImage(new System.Uri("ms-appx:///Assets/StoreLogo.png")),
+                }
+            };
+        }
+    }
+}
