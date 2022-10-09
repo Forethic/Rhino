f6d6972520d87c1348283d6cdf6e9e9210d2dcbf
Jonathan <Forethic@outlook.com>
2022-10-09 22:26:32 +0800

    Learn UWP - Inventory PART 3.4 - Dashboard Part - OrdersPane

---
+++ Learn/LearnUWP/InventorySample/Inventory.App/Models/OrderModel.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.ViewModels;
+using System;
+
+namespace Inventory.Models
+{
+    public class OrderModel : ModelBase
+    {
+        public static OrderModel CreateEmpty() => new OrderModel { OrderID = -1, CustomerID = -1, IsEmpty = true };
+
+        public long OrderID { get; set; }
+
+        public long CustomerID { get; set; }
+
+        public DateTimeOffset OrderDate { get; set; }
+    }
+}
---
+++ Learn/LearnUWP/InventorySample/Inventory.App/Views/Dashboard/Parts/OrdersPane.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Inventory.Models;
+using System.Collections.Generic;
+using Windows.UI.Xaml;
+
+namespace Inventory.Views
+{
+    public sealed partial class OrdersPane
+    {
+        public IList<OrderModel> ItemsSource
+        {
+            get => (IList<OrderModel>)GetValue(ItemsSourceProperty);
+            set => SetValue(ItemsSourceProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemsSourceProperty
+            = DependencyProperty.Register(nameof(ItemsSource), typeof(IList<OrderModel>), typeof(OrdersPane), new PropertyMetadata(default));
+
+        public OrdersPane()
+        {
+            InitializeComponent();
+
+            ItemsSource = new List<OrderModel>()
+            {
+                new OrderModel()
+                {
+                    OrderID = 10001,
+                    CustomerID = 99986,
+                    OrderDate = System.DateTimeOffset.Now,
+                }
+            };
+        }
+    }
+}
